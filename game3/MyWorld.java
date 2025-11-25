8b56e4ab8d47e70ed460ad2887f4f3973390fbd8
5sj17 <21193@hakodate.kosen-ac.jp>
2025-11-25 10:26:14 +0900

    背景

--- game3/MyWorld.java
+++ game3/MyWorld.java
@@ -16,15 +16,10 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-<<<<<<< HEAD
+
         super(851, 567, 1); 
         showText( "サイコロクエスト", 450, 300 );
         showText( "Spaceを押してスタート", 450, 450 );
-=======
-        super(600, 400, 1); 
-        
-        
         showText( "1", 300, 50 );
->>>>>>> bd7ebce831884d6f3c0fe98e1e7f859d0f306997
     }
 }
