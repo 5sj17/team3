d6f968e6332f2170778278130e5ffa2eed9bf690
5SJ18 <21092@hakodate.kosen-ac.jp>
2025-12-09 09:04:57 +0900

    たろう更新1

--- game3/MyWorld.java
+++ game3/MyWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private boolean taroAdded = false;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -19,11 +19,14 @@ public class MyWorld extends World
 
         super(851, 567, 1); 
         showText( "サイコロクエスト", 450, 300 );
-        showText( "Spaceを押してスタート", 450, 450 );
-
-      
-
-   
-     
+        showText( "Spaceを押してスタート", 450, 450 ); 
+    }
+        public void act()
+    {
+        if (Greenfoot.isKeyDown("space") && !taroAdded)
+        {
+            addObject(new Taro(), 300, 400); // 座標(300, 400)にTaroオブジェクトを追加
+            taroAdded = true; // フラグを立てて、二度追加されないようにする
+        }
     }
 }
