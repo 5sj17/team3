a70144dcaf54964d2fd916bd2622e39bd361ef9c
ebio629 <21017@hakodate.kosen-ac.jp>
2025-12-09 10:25:01 +0900

    確認ステータス途中

--- game3/MyWorld.java
+++ game3/MyWorld.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     private boolean taroAdded = false;
+    
+    private int currentHp = 3;//ハートの初期値,現在
+    private final int MAX_HP = 3;//最大HP
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -28,19 +31,52 @@ public class MyWorld extends World
         {
             addObject(new Taro(), 150, 400); 
             taroAdded = true; // 二度追加されないようにする
+            status_prepare();
         }
         if (Greenfoot.isKeyDown("space"))
         {
             showText( "", 450, 300 );
             showText( "", 450, 450 ); 
+            
         }
      
-        addObject( new status_bw_heart(), 70, 50 );//ステータス表示
-        addObject( new status_heart(), 200, 50 );
-        addObject( new status_heart(), 330, 50 );
-
+        //addObject( new status_bw_heart(), 70, 50 );//ステータス表示
+        //addObject( new status_heart(), 200, 50 );
+        //addObject( new status_heart(), 330, 50 );
+        
         
 
     }
+    public void status_prepare()
+    {
+        for(int i=0;i<MAX_HP;i++){
+            addObject(new status_heart(),70+(130*i),50);
+        }
+    }
+    public void status_damage()
+    {
+        if(currentHp>0){
+            currentHp--;
+            switch(currentHp){
+                case 2://削除動作作る
+                //removeObject(new status_heart(),70+(130*2),50);
+                addObject(new status_bw_heart(),70+(130*2),50);
+                break;
+                case 1:
+                //removeObject(new status_heart(),70+(130*1),50);
+                addObject(new status_bw_heart(),70+(130*1),50);
+                break;
+                case 0://ゲームオーバー
+                //removeObject(new status_heart(),70+(130*0),50);
+                addObject(new status_bw_heart(),70+(130*0),50);
+                Greenfoot.stop();
+                break;
+                
+        }
+    }      
+    }
+    //public void status_heal()
+    //{
+    //}
 }
 
