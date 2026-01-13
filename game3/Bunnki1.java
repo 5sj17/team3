8feb5f17acda100b52f5f8bec472e1817f0a17f4
kira171018 <21024@hakodate.kosen-ac.jp>
2026-01-13 09:47:44 +0900

    bunkisyori

--- game3/Bunnki1.java
+++ game3/Bunnki1.java
@@ -7,16 +7,8 @@ public class Bunnki1 extends Actor
     public Bunnki1()
     {
         baseImg = new GreenfootImage("分岐1.png");
-        baseImg.scale(800,650);   // ←  最初から縮小しておく
+        baseImg.scale(600,450);   // ←  最初から縮小しておく
         setImage(baseImg);        // ← 初期表示も正しい大きさになる
     }
-        public void act() 
-    {
-        // スペースキーが押されたか、かつまだ画像が表示されていないかを確認
-        if (Greenfoot.isKeyDown("space") && !imageDisplayed)
-        {
-            setImage(baseImg); // 画像をセットして表示
-            imageDisplayed = true; // 画像が表示されたことを記録
-        } 
-    }        
-}
+    
+}        
