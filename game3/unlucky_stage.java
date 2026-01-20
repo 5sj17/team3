a2b5704070903ace3ba29db5f9138f345140df82
5sj17 <21193@hakodate.kosen-ac.jp>
2026-01-20 09:26:40 +0900

    atougou

--- game3/unlucky_stage.java
+++ game3/unlucky_stage.java
@@ -17,6 +17,5 @@ public class unlucky_stage extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
     }
 }
